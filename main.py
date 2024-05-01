5395fa035f506be481bd95acb2cd966cd3be9544
Amrit Rai <iamamrit27@gmail.com>
2024-05-01 21:04:16 +0530

    error 405

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import tweepy
+import matplotlib.pyplot as plt
+import pandas as pd
+from wordcloud import WordCloud
+from wordcloud import STOPWORDS
+ 
+from dotenv import load_dotenv
+import os
 
-consumer_key = "XXXXX"
-consumer_secret = "XXXXX"
+# Load environment variables from .env file
+load_dotenv()
+
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+hf_token = os.getenv("HF_TOKEN")
 
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
@@ -19,7 +30,7 @@ def limit_handled(cursor):
             break
  
 # Define the term you will be using for searching tweets
-query = '@NotionHQ'
+query = '@@rabbit_hmi'
 query = query + ' -filter:retweets'
  
 # Define how many tweets to get from the Twitter API
@@ -37,3 +48,60 @@ tweets = []
 for result in search:
     tweet_content = result.full_text
     tweets.append(tweet_content) # Only saving the tweet content.
+
+model = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+API_URL = "https://api-inference.huggingface.co/models/" + model
+headers = {"Authorization": "Bearer %s" % (hf_token)}
+
+def analysis(data):
+    payload = dict(inputs=data, options=dict(wait_for_model=True))
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+tweets_analysis = []
+for tweet in tweets:
+    try:
+        sentiment_result = analysis(tweet)[0]
+        top_sentiment = max(sentiment_result, key=lambda x: x['score']) # Get the sentiment with the higher score
+        tweets_analysis.append({'tweet': tweet, 'sentiment': top_sentiment['label']})
+ 
+    except Exception as e:
+        print(e)
+ 
+# Load the data in a dataframe
+pd.set_option('max_colwidth', None)
+pd.set_option('display.width', 3000)
+df = pd.DataFrame(tweets_analysis)
+ 
+# Show a tweet for each sentiment
+display(df[df["sentiment"] == 'Positive'].head(1))
+display(df[df["sentiment"] == 'Neutral'].head(1))
+display(df[df["sentiment"] == 'Negative'].head(1))
+
+
+sentiment_counts = df.groupby(['sentiment']).size()
+print(sentiment_counts)
+
+fig = plt.figure(figsize=(6,6), dpi=100)
+ax = plt.subplot(111)
+sentiment_counts.plot.pie(ax=ax, autopct='%1.1f%%', startangle=270, fontsize=12, label="")
+
+# Wordcloud with positive tweets
+positive_tweets = df['tweet'][df["sentiment"] == 'Positive']
+stop_words = ["https", "co", "RT"] + list(STOPWORDS)
+positive_wordcloud = WordCloud(max_font_size=50, max_words=50, background_color="white", stopwords = stop_words).generate(str(positive_tweets))
+plt.figure()
+plt.title("Positive Tweets - Wordcloud")
+plt.imshow(positive_wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+ 
+# Wordcloud with negative tweets
+negative_tweets = df['tweet'][df["sentiment"] == 'Negative']
+stop_words = ["https", "co", "RT"] + list(STOPWORDS)
+negative_wordcloud = WordCloud(max_font_size=50, max_words=50, background_color="white", stopwords = stop_words).generate(str(negative_tweets))
+plt.figure()
+plt.title("Negative Tweets - Wordcloud")
+plt.imshow(negative_wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
